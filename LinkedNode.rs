e19e2e12af0e5e0311ec2b725fe162bb778fff2b
Hongzheng Wang <wanghz@gmail.com>
2023-01-14 10:29:04 -0800

    Create LinkedNode.rs

---
+++ LinkedNode.rs
@@ -0,0 +1,39 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(value: i32) -> Self {
+        Node { value, next: None }
+    }
+
+    fn append(&mut self: &mut Self, value: i32) {
+        while let Some(ref mut x) = self.next {
+            self = x;
+        }
+        self.next = Some(Box::new(Self::new(value)));
+    }
+
+    fn prepend(&mut self, value: i32) {
+        let old_val = self.value;
+        let old_tail = self.next.take();
+        self.next = Some(Box::new(Node {
+            value: old_val,
+            next: old_tail,
+        }));
+        self.value = value;
+    }
+}
+
+fn main() {
+    let mut n = Node::new(12);
+    n.append(13);
+    n.append(14);
+    n.prepend(11);
+    n.append(15);
+    n.prepend(10);
+    println!("{:#?}", n);
+}
